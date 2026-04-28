edbd5a20ecc693fdce2300463874d0e0336e0b80
daniel luke <manthid5@gmail.com>
2026-04-28 10:40:40 +0300

    Initialize header styling and set up DOM elements for interaction

---
+++ code.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+header.style.backgroundColor = "blue";
+
+const h1 = document.querySelector("h1");
+const headerParagraph = document.querySelector("#header-p");
+const button = document.querySelector("#submit-button");
+let CC;
+let YY;
+let MM;
+let DD;
+let d;
+
+
